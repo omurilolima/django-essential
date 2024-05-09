6ab203a23b98a5dbee6aea16ac6cde92450b4488
omurilolima <muriloserafimlima@gmail.com>
2024-05-09 12:28:02 +0100

    Add notes to admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
 
 # Register your models here.
+admin.site.register(models.Notes, NotesAdmin)
