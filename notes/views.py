783aadd8637483345a2daddd18c148146d7c2891
omurilolima <muriloserafimlima@gmail.com>
2024-05-10 19:02:47 +0100

    Display only the logged user data

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,10 @@
-from typing import List
+from typing import Any, List
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -26,10 +28,14 @@ class NotesCreateView(CreateView):
     form_class = NotesForm
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 
 class NotesDetailView(DetailView):
